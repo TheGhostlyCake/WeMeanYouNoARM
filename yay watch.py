1ec5738d9b05c866abfe26d090387104afd58587
sciguy16 <david@thedavidyoung.co.uk>
2016-12-03 23:10:29 +0000

    the game

--- yay watch.py
+++ yay watch.py
@@ -3,6 +3,7 @@ import neopixel
 import music
 import random
 MUSIC_PIN = pin1
+CLOCK_LEVEL = 100
 uart.init(baudrate=115200,tx=None,rx=pin2)
 musics=[
     music.DADADADUM,
@@ -10,7 +11,6 @@ musics=[
     music.PRELUDE,
     music.ODE,
     music.NYAN,
-    music.RINGTONE,
     music.BIRTHDAY,
     music.WEDDING,
     music.FUNERAL,
@@ -18,6 +18,8 @@ musics=[
 # Setup the Neopixel strip on pin0 with a length of 8 pixels
 np = neopixel.NeoPixel(pin0, 12)
 compassMode=False
+leds_crazy=False
+led_level=0
 compass.calibrate()
 while True:
     line=uart.readline()
@@ -26,19 +28,20 @@ while True:
         # string looks like ;[a-zA-Z0-9]*
         uart.write(line)
         display.scroll(str(line)[3:-2])
-    elif((line != None and line[0]==124) or button_b.was_pressed()): # 124=|
+    elif(line != None and line[0]==124): # 124=|
+        button_b.was_pressed()
         music.play(musics[random.randrange(len(musics))],pin=MUSIC_PIN,wait=False,loop=True)
+        leds_crazy=True
     elif(line != None and len(line)>1 and b',' in line and not compassMode):
       try:
         uart.write(line)
-        uart.write(str(temperature()))
         # string looks like [0-9][0-9]?,[0-9][0-9]?,[0-9][0-9]?\n
         spl=line.split(b',')
-        red=int(spl[0])-1
-        green=int(spl[1])-1
-        blue=int(spl[2])-1
+        red=(int(spl[0])-1+5)%12
+        green=(int(spl[1])-1+5)%12
+        blue=(int(spl[2])-1+5)%12
         for num in range(len(np)):
-            np[num] = (255 if num==red else 0,255 if num==green else 0,255 if num==blue else 0)
+            np[num] = (CLOCK_LEVEL if num==red else 0,CLOCK_LEVEL if num==green else 0,CLOCK_LEVEL if num==blue else 0)
         np.show()
         sleep(10)
       except:
@@ -53,14 +56,20 @@ while True:
             compassMode=False
         elif(not compassMode):
             # enable compass mode
-            uart.write("\r\ndisable compass\r\n")
+            uart.write("\r\nenable compass\r\n")
             compassMode=True
     if(compassMode):
-            uart.write("\r\nget heading\r\n")
+#            uart.write("\r\nget heading\r\n")
             heading=compass.heading()
-            heading=round(heading/30)
+            heading=(-round(heading/30)+4)%12
             for num in range(len(np)):
-                np[num] = (20 if num==heading else 0,255 if num==heading else 0,20 if num==heading else 0)
+                np[num] = (20 if num==heading else 0,0,20 if num==heading else 0)
             np.show()
     if(button_b.was_pressed()):
-        uart.write("\nbutton b\n")
+        music.stop(MUSIC_PIN)
+        leds_crazy=False
+    if(leds_crazy):
+        led_level=0 if led_level==255 else 255
+        for num in range(len(np)):
+            np[num]=(led_level,led_level,led_level)
+        np.show()
