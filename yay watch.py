94110b3bf59588ae6c9df3c4236de203edeb5ee4
sciguy16 <david@thedavidyoung.co.uk>
2016-12-04 01:04:45 +0000

    yay
    (with all of the effects)

--- yay watch.py
+++ yay watch.py
@@ -1,9 +1,24 @@
+# SET COMPASS CALIBRATE!!!!!!!!!!!!!
 from microbit import *
 import neopixel
 import music
 import random
 MUSIC_PIN = pin1
 CLOCK_LEVEL = 100
+NOTIFICATION_COLOUR=[
+(0x89,0x00,0x00),
+(0x58,0x3D,0x00),
+(0x37,0x89,0x00),
+(0x00,0x88,0x14),
+(0x00,0x47,0x4A),
+(0x00,0x38,0x80),
+(0x00,0x00,0x95),
+(0x17,0x00,0x86),
+(0x39,0x00,0x65),
+(0x48,0x00,0x37),
+(0x7D,0x00,0x17),
+(0x85,0x00,0x08)]
+
 uart.init(baudrate=115200,tx=None,rx=pin2)
 musics=[
     music.DADADADUM,
@@ -20,14 +35,46 @@ np = neopixel.NeoPixel(pin0, 12)
 compassMode=False
 leds_crazy=False
 led_level=0
-compass.calibrate()
+#compass.calibrate()
+
+def fun(i):
+ for num in range(len(np)):
+    np[num]=NOTIFICATION_COLOUR[num]
+    np.show()
+    sleep(80)
+ for nom in range(i*12):
+  for num in range(len(np)):
+      np[(num+nom)%12] = NOTIFICATION_COLOUR[num]
+  np.show()
+  sleep(80)
+ for num in range(len(np)):
+    np[num]=(0,0,0)
+    np.show()
+    sleep(80)
+#fun(14)
+
+music.play(music.POWER_UP,pin=MUSIC_PIN,wait=False,loop=False)
+fun(2)
+
 while True:
-    line=uart.readline()
+    buttonA=button_a.was_pressed()
+    line=None
+    if(uart.any()):
+        line=uart.readline()
     #line="12,4\n"
     if(line != None and line[0]==59): # 59 = ;
         # string looks like ;[a-zA-Z0-9]*
+        music.play(music.POWER_UP,pin=MUSIC_PIN,wait=False,loop=False)
+        for num in range(len(np)):
+            np[num]=(0,0,0)
+        np.show()
+        for num in range(len(np)):
+            np[num]=NOTIFICATION_COLOUR[num]
+            np.show()
+            sleep(80)
         uart.write(line)
-        display.scroll(str(line)[3:-2])
+        display.scroll(str(line)[3:-3],wait=False,monospace=True)
+        fun(round(.78*len(str(line)[3:-3])))
     elif(line != None and line[0]==124): # 124=|
         button_b.was_pressed()
         music.play(musics[random.randrange(len(musics))],pin=MUSIC_PIN,wait=False,loop=True)
@@ -47,17 +94,19 @@ while True:
       except:
         uart.write("\r\ni crash :(\r\n")
         
-    if(button_a.was_pressed()):
+    if(buttonA):
         uart.write("\nbutton a\n")
         # enable compass mode
         if(compassMode):
             # disable compass mode
             uart.write("\r\ndisable compass\r\n")
             compassMode=False
+            sleep(10)
         elif(not compassMode):
             # enable compass mode
             uart.write("\r\nenable compass\r\n")
             compassMode=True
+            sleep(10)
     if(compassMode):
 #            uart.write("\r\nget heading\r\n")
             heading=compass.heading()
